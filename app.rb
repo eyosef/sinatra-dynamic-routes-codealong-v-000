f72437ac9da703027e6739d946a0a57e945074f0
Eden Mesfun <emesfun@gmail.com>
2018-03-21 17:42:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,8 +19,8 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    @product = params[:num1] * params[:num2]
-    "#{product}"
+    # @product = params[:num1] * params[:num2]
+    # "#{product}"
   end
 
 end
